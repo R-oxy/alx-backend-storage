4a1bd9b35d0d60ffccd87edf0f0294ff035445b6
R-oxy <aronmangati@gmail.com>
2024-06-13 03:19:14 -0700

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,32 +3,16 @@
 
 DELIMITER //
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser (
     IN user_id INT
 )
-BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
-    DECLARE avg_score FLOAT;
-    
-    -- Calculate total score and total number of projects
-    SELECT SUM(score), COUNT(*)
-    INTO total_score, total_projects
-    FROM corrections
-    WHERE user_id = user_id;
-    
-    -- Compute average score
-    IF total_projects > 0 THEN
-        SET avg_score = total_score / total_projects;
-    ELSE
-        SET avg_score = 0; -- default average score if no projects
-    END IF;
-    
-    -- Update average_score for the user
+BEGIN 
     UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-    
+    SET average_score = (SELECT AVG(score)
+                        FROM corrections
+                        WHERE corrections.user_id = user_id)
+    WHERE id = user_id;    
 END //
 
 DELIMITER ;
